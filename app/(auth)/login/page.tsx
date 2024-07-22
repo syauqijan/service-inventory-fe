9648193b065d3a9e8bdbd062939ce5dc99a147b9
syauqijan <13521014@std.stei.itb.ac.id>
2024-07-22 14:34:27 +0700

    feat: update login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react';
+import useAuth from '../../../hooks/useAuth';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import logoImage from '@/public/login-image.svg'
@@ -7,10 +8,29 @@ import logoPlaceholder from '@/public/logo.svg'
 
 const Login = () => {
   const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login } = useAuth();
 
-  const handleLoginClick = () => {
-    router.push('/dashboard');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault();
+      const response = await fetch('http://localhost:8000/login', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+          login(data.token, data.expiresIn);
+          router.push('/dashboard');
+      } else {
+          console.error(data.msg);
+      }
   };
+
   return (
     <div className='flex min-w-screen min-h-screen bg-neutral-100 justify-center items-center text-black'>
       <div className="w-[887px] h-[441px] px-[42px] py-[32px] bg-white text-black rounded-3xl shadow flex-row justify-start items-start gap-[38px] inline-flex">
@@ -18,6 +38,8 @@ const Login = () => {
           <Image src={logoImage} width={375} height={375} alt='logo-image'/>
         </div>
         <div className='w-[418px] flex-col justify-center items-start self-center gap-2.5 inline-flex'>
+          <form onSubmit={handleSubmit}>
+
           <Image src={logoPlaceholder} width={150} height={50} alt='logo-image'/>
 
           <div className='text-2xl font-semibold pt-2'>
@@ -25,17 +47,27 @@ const Login = () => {
           </div>
           <div className='w-full flex-col justify-start items-start gap-1.5'>
             <label htmlFor="email" className='text-sm font-medium'>Email</label>
-            <input type="email" placeholder='Enter your email' name="email" id="email" className='w-full border border-slate-300 rounded-md self-stretch pl-3 py-2' />
+            <input 
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Enter your email' name="email" id="email" className='w-full border border-slate-300 rounded-md self-stretch pl-3 py-2' />
 
           </div>
           <div className='w-full'>
             <label htmlFor="password" className='text-sm font-medium'>Password</label>
-            <input type="password" placeholder='Enter your password' name="password" id="password" className='w-full border border-slate-300 rounded-md self-stretch pl-3 pr-14 py-2' />
+            <input 
+              type="password" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Enter your password' name="password" id="password" className='w-full border border-slate-300 rounded-md self-stretch pl-3 pr-14 py-2' />
           </div>
-          <div className='flex w-full h-10 px-4 py-2 bg-RedTint/900 rounded-md justify-center items-center mt-4 ' onClick={handleLoginClick}>
-            <button className=' w-full text-white rounded-[10px] p-2' >Login</button>
+          <div className='flex w-full h-10 px-4 py-2 bg-RedTint/900 rounded-md justify-center items-center mt-4 '>
+            <button type="submit" className=' w-full text-white rounded-[10px] p-2' >Login</button>
           </div>
+          </form>
           </div>
+
       </div>
       
     </div>

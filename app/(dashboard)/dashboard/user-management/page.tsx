8b4482ea4b9472a50f7bc8e3c26b8032d8d547b1
syauqijan <13521014@std.stei.itb.ac.id>
2024-07-23 16:06:36 +0700

    feat: update fetch data table

--- app/(dashboard)/dashboard/user-management/page.tsx
+++ app/(dashboard)/dashboard/user-management/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserTable } from '@/components/tables/user-tables/user-tables';
 import { ColumnDef } from '@tanstack/react-table';
 import { Breadcrumbs } from '@/components/breadcrumbs';
@@ -9,9 +9,12 @@ import PopupUser from '@/components/popup/popup-user';
 import { CellAction } from '@/components/tables/user-tables/cell-actions';
 
 interface User {
+  id: number;
   name: string;
   email: string;
-  role: string;
+  role: {
+    name: string;
+  } | null;
 }
 
 const columns: ColumnDef<User, any>[] = [
@@ -24,41 +27,53 @@ const columns: ColumnDef<User, any>[] = [
     header: 'Email',
   },
   {
-    accessorKey: 'role',
+    accessorKey: 'role.name', // Nested accessor for role name
     header: 'Role',
   },
   {
     accessorKey: 'actions',
     header: 'Actions',
-    cell: () => <CellAction /> //TO DO: Add the correct data properties
+    cell: () => <CellAction />, // TODO: Add the correct data properties
   },
 ];
 
-const data: User[] = [
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Admin' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Developer' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Project Manager' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Admin' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Admin' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Admin' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Developer' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Project Manager' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Admin' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Admin' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Admin' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Developer' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Project Manager' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Admin' },
-  { name: 'Is Mail', email: 'imail@mail.com', role: 'Admin' },
+const breadcrumbItems = [
+  { title: 'Main', link: '/dashboard' },
+  { title: 'User Management', link: '/dashboard/user-management' },
 ];
 
-const breadcrumbItems = [
-    { title: 'Main', link: '/dashboard' },
-    { title: 'User Management', link: '/dashboard/user-management' }
-  ];
-  
 const Page = () => {
   const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const [data, setData] = useState<User[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT_USERS || '', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          const errorText = await response.text();
+          console.error('Error fetching users:', errorText);
+          setError('Failed to fetch users');
+          return;
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        setError('An error occurred while fetching users');
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleAddNewClick = () => {
     setIsPopupVisible(true);
@@ -67,23 +82,23 @@ const Page = () => {
   const handleClosePopup = () => {
     setIsPopupVisible(false);
   };
-return (
-<>
-    <div className="flex-1 space-y-4  p-4 pt-6 md:p-8">
+
+  return (
+    <>
+      <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
         <Breadcrumbs items={breadcrumbItems} />
         <div className="flex items-start justify-between">
-          <Heading
-            title="User Management"
-            description="Create and manage user account"
-          />
+          <Heading title="User Management" description="Create and manage user account" />
           <button
             onClick={handleAddNewClick}
-            className='w-[118px] h-10 px-4 py-2 bg-RedTint/900 rounded-md justify-center items-center inline-flex text-white text-sm font-medium'
+            className="w-[118px] h-10 px-4 py-2 bg-RedTint/900 rounded-md justify-center items-center inline-flex text-white text-sm font-medium"
           >
             <Plus className="mr-2 h-4 w-4" /> Add New
           </button>
         </div>
         <hr className="border-neutral-200" />
+
+        {error && <div className="text-red-500">{error}</div>}
 
         <UserTable
           columns={columns}

cb84eb4a408c0ce64b08b84056aa2789a16ae172
syauqijan <13521014@std.stei.itb.ac.id>
2024-07-30 17:09:51 +0700

    feat: layout service page

--- app/(dashboard)/dashboard/service/page.tsx
+++ app/(dashboard)/dashboard/service/page.tsx
@@ -1,165 +1,127 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { UserTable } from '@/components/tables/user-tables/user-tables';
+import { ColumnDef } from '@tanstack/react-table';
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import { Heading } from '@/components/ui/heading';
-import { useRouter } from 'next/navigation';
-
+import { Plus } from 'lucide-react';
+import PopupUser from '@/components/popup/popup-user';
+import DeleteUser from '@/components/modal/delete-user';
+import UpdateUser from '@/components/popup/update-user';
+import { CellAction } from '@/components/tables/user-tables/cell-actions';
+import { toast } from 'sonner';
+import { Input } from '@/components/ui/input';
+import { SkeletonTable } from '@/components/tables/skeleton-tables';
+import { useDebounce } from '@/hooks/useDebounce'; 
+import {getColumns} from '@/components/tables/user-tables/columns';
+import { useSearchParams } from 'next/navigation';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import Link from 'next/link';
 
 const breadcrumbItems = [
     { title: 'Main', link: '/dashboard' },
     { title: 'Service', link: '/dashboard/service' },
-    { title: 'Add New Service', link: '/dashboard/service-detail' }
-  ];
- 
-interface Status {
-    statusName: string;
-  }  
+];
+
+
 
 const Page = () => {
-    const router = useRouter();
-    const [isChecked, setIsChecked] = useState(false)
-    const [isChecked2, setIsChecked2] = useState(false)
+    const [isPopupVisible, setIsPopupVisible] = useState(false);
+    const [isModalDeleteVisible, setIsModalDeleteVisible] = useState(false);
+    const [isModalUpdateVisible, setIsModalUpdateVisible] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    // const [selectedUser, setSelectedUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [totalUsers, setTotalUsers] = useState<number>(0);
+    const searchParams = useSearchParams();
+    const debouncedSearchTerm = useDebounce(searchTerm, 500);
+    // const page = Number(searchParams.get('page') ?? '1');
+    // const limit = Number(searchParams.get('limit') ?? '10');
+    const [page, setPage] = useState<number>(1);
+    const [limit, setLimit] = useState<number>(10);
 
-    const [userId, setUserId] = useState<string>('');
-    const [gitlabUrl, setGitlabUrl] = useState<string>('');
-    const [name, setName] = useState<string>('');
-    const [description, setDescription] = useState<string>('');
-    const [preprodUrl, setPreprodUrl] = useState<string>('');
-    const [preprodUrlStatus, setPreprodUrlStatus] = useState<string>('');
-    const [prodUrl, setProdUrl] = useState<string>('');
-    const [prodUrlStatus, setProdUrlStatus] = useState<string>('');
-
-
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-
-    const createSubmit = async () => {
-        setIsLoading(true);
-        const response = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT_SERVICE||'', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ 
-            name, gitlabUrl, description, preprodUrl, preprodUrlStatus,
-            prodUrl, prodUrlStatus, userId
-         }),
-        });
-        if (response.ok) {
-        console.log('Service Created');
-        } else {
-            console.error('Failed to create service');
-        }
+    const handleAddNewClick = () => {
+        setIsPopupVisible(true);
     };
-
-    const handleCheckboxChange = () => {
-        setIsChecked(!isChecked)
-      }
-    
-    const handleCheckboxChange2 = () => {
-        setIsChecked2(!isChecked2)
-      }
-
-    return(
-        <div className='flex max-h-screen overflow-y-auto '>
-            <div className="flex-1 space-y-4  p-4 md:p-8">
-                <Breadcrumbs items={breadcrumbItems} />
-                <div className="flex items-start justify-between">
-                    <Heading
-                        title="Add New Service Web"
-                        description="Add a new service"
-                    />
-                </div>
-                <hr className="border-neutral-200" />
-                <div className='text-sm'>
-                    <form>
-                        <div className='w-2/5'>
-                            <h3 className='font-medium mb-1'>Service Name</h3>
-                            <input type="text" id="Updateemail" name="Updateemail" value={name} onChange={(e) => setName(e.target.value)}
-                            placeholder="Enter Service Name" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
-                        </div>                        
-                        <div className='w-2/5'>
-                            <h3 className='font-medium mb-1'>Gitlab url</h3>
-                            <input type="text" id="Updateemail" name="Updateemail" value={gitlabUrl} onChange={(e) => setGitlabUrl(e.target.value)}
-                            placeholder="Enter Service Name" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
-                        </div>
-                        <div className='w-2/5'>
-                            <h3 className='font-medium mb-1'>User ID</h3>
-                            <input type="text" id="Updateemail" name="Updateemail" value={userId} onChange={(e) => setUserId(e.target.value)}
-                            placeholder="Enter Service Name" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
-                        </div>
-                        <div className='mt-4'>
-                            <h3 className='font-medium mb-1'>Description</h3>
-                            
-                            <input type="text" id="Updateemail" name="Updateemail" value={description} onChange={(e) => setDescription(e.target.value)}
-                            placeholder="Enter Link" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
-                        </div>
-
-                        <div className='mt-3 flex justify-center w-1/2'>
-                            <div className='w-4/5'>
-                                <h3 className='font-medium mb-1'>Pre-Prod URL</h3>
-                                <input type="text" id="Updateemail" name="Updateemail" value={preprodUrl} onChange={(e) => setPreprodUrl(e.target.value)}
-                                placeholder="Enter Link" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
-                            </div>
-                            <div className='w-1/5'>
-                                <p className='mb-2 ml-1 font-medium'>
-                                    Status
-                                </p>
-                                <label className='autoSaverSwitch relative inline-flex cursor-pointer select-none items-center'>
-                                    <input type='checkbox' name='autoSaver' className='sr-only' checked={isChecked} onChange={handleCheckboxChange} />
-                                    <span className={`slider mr-3 flex h-[26px] w-[50px] items-center rounded-full p-1 duration-200 ${ isChecked ? 'bg-red-600' : 'bg-[#CCCCCE]' }`}>
-                                        <span
-                                            className={`dot h-[18px] w-[18px] rounded-full bg-white duration-200 ${
-                                            isChecked ? 'translate-x-6' : ''
-                                            }`}>
-                                        </span>
-                                    </span>
-                                    <span className='label flex items-center text-sm font-medium text-black'>
-                                        <span className='pl-1'>
-                                            <input type='text' id='preprodUrlStatus' value={isChecked ? 'Active' : 'Inactive'} onChange={(e) => setPreprodUrlStatus(e.target.value)}>
-                                            </input>
-                                        </span>
-                                    </span>
-                                </label>
-                            </div>
-                        </div>
-                        <div className='mt-6 flex justify-center w-1/2'>
-                            <div className='w-4/5'>
-                                <h3 className='font-medium mb-1'>Production URL</h3>
-                                <input type="text" id="Updateemail" name="Updateemail" placeholder="Enter Link" value={prodUrl} onChange={(e) => setProdUrl(e.target.value)}
-                                className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
-                            </div>
-                            <div className='w-1/5'>
-                                <p className='mb-2 ml-1 font-medium'>
-                                    Status
-                                </p>
-                                <label className='autoSaverSwitch relative inline-flex cursor-pointer select-none items-center'>
-                                    <input type='checkbox' name='autoSaver' className='sr-only' checked={isChecked2} onChange={handleCheckboxChange2} />
-                                    <span className={`slider mr-3 flex h-[26px] w-[50px] items-center rounded-full p-1 duration-200 ${ isChecked2 ? 'bg-red-600' : 'bg-[#CCCCCE]' }`}>
-                                        <span
-                                            className={`dot h-[18px] w-[18px] rounded-full bg-white duration-200 ${
-                                            isChecked2 ? 'translate-x-6' : ''
-                                            }`}>
-                                        </span>
-                                    </span>
-                                    <span className='label flex items-center text-sm font-medium text-black'>
-                                        <span className='pl-1'>
-                                            <input type='text' id='prodUrlStatus' value={isChecked2 ? 'Active' : 'Inactive'} onChange={(e) => setProdUrlStatus(e.target.value)}>
-                                            </input>
-                                        </span>
-                                    </span>
-                                </label>
-                            </div>
-                        </div>
-                        <div className='mt-8 flex justify-between pr-16'>
-                            <p className='active:scale-95 min-w-16 form-flex justify-center items-center font-normal border py-3 px-4 gap-2 cursor-pointer rounded-md shadow-sm text-red-600 bg-white border border-red-600 w-20 mt-3 mb-1 ml-3 font-semibold text-center' onClick={() => router.back()}>Back</p>
-                            <input type="submit" value="Save" className="active:scale-95 min-w-16 form-flex justify-center items-center font-normal border py-3 px-4 gap-2 cursor-pointer rounded-md shadow-sm text-white bg-red-600 w-20 mt-3 mb-1 ml-3 font-semibold" 
-                                onClick={createSubmit}
-                            />
-                        </div>
-                    </form>
-                </div>
+    return (
+        <>
+        <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
+            <Breadcrumbs items={breadcrumbItems} />
+            <div className="flex items-start justify-between">
+            <Heading
+                title="Service"
+                description="Create and manage services"
+            />
+            
+            <Link
+                href={'/dashboard/service/service-web'}
+                className="w-[118px] h-10 px-4 py-2 bg-RedTint/900 rounded-md justify-center items-center inline-flex text-white text-sm font-medium"
+            >
+                <Plus className="mr-2 h-4 w-4" /> Add New
+            </Link>
             </div>
+            <hr className="border-neutral-200" />
+
+            {error && <div className="text-red-500">{error}</div>}
+            <div className='flex flex-row justify-between'>
+                <div>
+                    <Tabs defaultValue="overview" className="space-y-4">
+                        <TabsList>
+                            <TabsTrigger value="web">Web</TabsTrigger>
+                            <TabsTrigger value="api">API</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="web">
+                            <p>Web content</p>
+                        </TabsContent>
+                        <TabsContent value="api">
+                            <p>API content</p>
+                        </TabsContent>
+
+                    </Tabs>
+                </div>
+                <Input
+                placeholder="Search service"
+                value={searchTerm}
+                className="w-full md:max-w-sm mb-4"
+                onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+
+            {/* {loading ? (
+            <SkeletonTable />
+            ) : (
+            <UserTable
+                columns={getColumns(handleDeleteUser, handleUpdateUser)}
+                data={data}
+                searchKey="name"
+                pageNo={page}
+                totalUsers={totalUsers}
+                pageCount={Math.ceil(totalUsers / limit)}
+                pageSizeOptions={[10, 20, 30, 40, 50]}
+            />
+            )} */}
         </div>
-    )
+        {/* <PopupUser
+            isVisible={isPopupVisible}
+            onClose={handleClosePopup}
+            onCreate={fetchData}
+        />
+        <DeleteUser
+            isVisible={isModalDeleteVisible}
+            onClose={handleCloseDeleteModal}
+            onConfirm={handleConfirmDelete}
+        />
+        <UpdateUser
+            isVisible={isModalUpdateVisible}
+            onClose={handleCloseUpdateModal}
+            user={selectedUser}
+            onUpdate={fetchData}
+        /> */}
+    </>
+        
+    
+    );
 }
+
 export default Page;

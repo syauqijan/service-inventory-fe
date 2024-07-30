ec81a18d43554c9f0f2bf6d902ce4874ff51438d
syauqijan <13521014@std.stei.itb.ac.id>
2024-07-30 17:09:51 +0700

    feat: layout service page

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <div className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur">
       <nav className="flex h-14 items-center justify-between px-4">
-        <div className="hidden lg:block">
+        <div className="hidden lg:block pl-[12px]">
           <Link href="/dashboard"
           >
             <Image src={Telkomsel} alt="logo" width={150} height={50} />
@@ -16,7 +16,7 @@ export default function Header() {
         <div className='block lg:!hidden'>
         </div>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 pr-[16px]">
           <UserNav />
         </div>
       </nav>

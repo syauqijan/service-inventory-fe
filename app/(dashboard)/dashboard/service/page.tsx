4858648a4cb709cbac7807347fc32d38e59a0e2b
HasthoRahtomo <harwototo@gmail.com>
2024-07-31 15:12:04 +0700

    Updating Create Service Web

--- app/(dashboard)/dashboard/service/page.tsx
+++ app/(dashboard)/dashboard/service/page.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import { Heading } from '@/components/ui/heading';
 import { useRouter } from 'next/navigation';
+import useAuth from '@/hooks/useAuth';
 
 
 const breadcrumbItems = [
@@ -15,23 +16,26 @@ interface Status {
     statusName: string;
   }  
 
+  var i1 = 0;
+  var i2 = 0;
+
 const Page = () => {
+    const { user } = useAuth();
     const router = useRouter();
     const [isChecked, setIsChecked] = useState(false)
     const [isChecked2, setIsChecked2] = useState(false)
-
-    const [userId, setUserId] = useState<string>('');
+    const userId = user?.userId;
     const [gitlabUrl, setGitlabUrl] = useState<string>('');
     const [name, setName] = useState<string>('');
     const [description, setDescription] = useState<string>('');
     const [preprodUrl, setPreprodUrl] = useState<string>('');
-    const [preprodUrlStatus, setPreprodUrlStatus] = useState<string>('');
+    const [preprodUrlStatus, setPreprodUrlStatus] = useState<string>('inactive');
     const [prodUrl, setProdUrl] = useState<string>('');
-    const [prodUrlStatus, setProdUrlStatus] = useState<string>('');
+    const [prodUrlStatus, setProdUrlStatus] = useState<string>('inactive');
 
 
     const [isLoading, setIsLoading] = useState<boolean>(false);
-
+    var userIDform = user?.userId;
     const createSubmit = async () => {
         setIsLoading(true);
         const response = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT_SERVICE||'', {
@@ -45,18 +49,32 @@ const Page = () => {
          }),
         });
         if (response.ok) {
-        console.log('Service Created');
+        console.log('Service Created', prodUrlStatus, preprodUrlStatus);
         } else {
             console.error('Failed to create service');
         }
     };
 
     const handleCheckboxChange = () => {
-        setIsChecked(!isChecked)
+        setIsChecked(!isChecked);
+        i1++;
+        if(i1 / 2 == 0){
+            setProdUrlStatus('inactive');
+        }
+        else{
+            setProdUrlStatus('active');
+        };
       }
     
     const handleCheckboxChange2 = () => {
         setIsChecked2(!isChecked2)
+        i2++;
+        if(i2 / 2 == 0){
+            setPreprodUrlStatus('inactive');
+        }
+        else{
+            setPreprodUrlStatus('active');
+        };
       }
 
     return(
@@ -73,17 +91,16 @@ const Page = () => {
                 <div className='text-sm'>
                     <form>
                         <div className='w-2/5'>
-                            <h3 className='font-medium mb-1'>Service Name</h3>
+                            <h3 className='font-medium mb-1'>Service Name {user?.userId}</h3>
                             <input type="text" id="Updateemail" name="Updateemail" value={name} onChange={(e) => setName(e.target.value)}
                             placeholder="Enter Service Name" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
                         </div>                        
-                        <div className='mt-4'>
+                        <div className=' mt-3'>
                             <h3 className='font-medium mb-1'>Description</h3>
-                            
                             <textarea id="Updateemail" name="Updateemail" value={description} onChange={(e) => setDescription(e.target.value)}
-                            placeholder="Enter Link" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-1/2" required></textarea>
+                            placeholder="Enter Link" className="min-h-20 max-h-20 emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-1/2" required></textarea>
                         </div>
-                        <div className='w-2/5 mt-4'>
+                        <div className='w-2/5 mt-1'>
                             <h3 className='font-medium mb-1'>Gitlab url</h3>
                             <input type="text" id="Updateemail" name="Updateemail" value={gitlabUrl} onChange={(e) => setGitlabUrl(e.target.value)}
                             placeholder="Enter Service Name" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
@@ -116,7 +133,7 @@ const Page = () => {
                                 </label>
                             </div>
                         </div>
-                        <div className='mt-6 flex justify-center w-1/2'>
+                        <div className='mt-3 flex justify-center w-1/2'>
                             <div className='w-4/5'>
                                 <h3 className='font-medium mb-1'>Production URL</h3>
                                 <input type="text" id="Updateemail" name="Updateemail" placeholder="Enter Link" value={prodUrl} onChange={(e) => setProdUrl(e.target.value)}

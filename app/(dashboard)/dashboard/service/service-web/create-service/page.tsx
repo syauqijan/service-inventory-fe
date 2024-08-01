2db6925a587e21b2600acd584a0e904fc750787f
syauqijan <13521014@std.stei.itb.ac.id>
2024-08-01 14:07:16 +0700

    feat: create service

--- app/(dashboard)/dashboard/service/service-web/create-service/page.tsx
+++ app/(dashboard)/dashboard/service/service-web/create-service/page.tsx
@@ -4,6 +4,7 @@ import { Breadcrumbs } from '@/components/breadcrumbs';
 import { Heading } from '@/components/ui/heading';
 import { useRouter } from 'next/navigation';
 import useAuth from '@/hooks/useAuth';
+import { toast } from 'sonner';
 
 
 const breadcrumbItems = [
@@ -68,50 +69,60 @@ const Page = () => {
         setErrors(err);
         return isValid;
     }
-    const createSubmit = async () => {
+    const createSubmit = async (event:any) => {
+        event.preventDefault();
+        if (!validateForm()) {
+            return;
+        }
         setIsLoading(true);
-        const response = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT_SERVICES||'', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ 
-            name, gitlabUrl, description, preprodUrl, preprodUrlStatus,
-            prodUrl, prodUrlStatus, userId
-         }),
-        });
-        if (response.ok) {
-        console.log('Service Created', prodUrlStatus, preprodUrlStatus);
-        } else {
-            console.error('Failed to create service');
+        try {
+            const response = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT_SERVICES || '', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name, gitlabUrl, description, preprodUrl, preprodUrlStatus,
+                    prodUrl, prodUrlStatus, userId
+                }),
+            });
+    
+            if (response.ok) {
+                router.push('/dashboard/service');
+                toast.success('Service created successfully');
+            } else {
+                const errorData = await response.json();
+                setErrors(errorData.errors);
+            }
+        } catch (error) {
+            console.error('Failed to create service', error);
+            toast.error('An error occurred while creating service');
+        } finally {
+            setIsLoading(false);
         }
     };
-
-    const handleCheckboxChange = () => {
-        setIsChecked(!isChecked);
-        i1++;
-        if(i1 / 2 == 0){
-            setProdUrlStatus('inactive');
-        }
-        else{
-            setProdUrlStatus('active');
-        };
-      }
     
-    const handleCheckboxChange2 = () => {
-        setIsChecked2(!isChecked2)
-        i2++;
-        if(i2 / 2 == 0){
-            setPreprodUrlStatus('inactive');
-        }
-        else{
-            setPreprodUrlStatus('active');
-        };
-    }
 
-    return(
+    const handleCheckboxPreProd = () => {
+        setIsChecked(prevState => {
+            const newState = !prevState;
+            setProdUrlStatus(newState ? 'active' : 'inactive');
+            return newState;
+        });
+    }
+    
+    const handleCheckboxProd = () => {
+        setIsChecked2(prevState => {
+            const newState = !prevState;
+            setPreprodUrlStatus(newState ? 'active' : 'inactive');
+            return newState;
+        });
+    }
+    
+
+    return (
         <div className='flex max-h-screen overflow-y-auto '>
-            <div className="flex-1 space-y-4  p-4 md:p-8">
+            <div className="flex-1 space-y-4 p-4 md:p-8">
                 <Breadcrumbs items={breadcrumbItems} />
                 <div className="flex items-start justify-between">
                     <Heading
@@ -121,39 +132,41 @@ const Page = () => {
                 </div>
                 <hr className="border-neutral-200" />
                 <div className='text-sm'>
-                    <form>
+                    <form onSubmit={createSubmit}>
                         <div className='w-2/5'>
                             <h3 className='font-medium mb-1'>Service Name</h3>
                             <input type="text" id="Updateemail" name="Updateemail" value={name} onChange={(e) => setName(e.target.value)}
-                            placeholder="Enter Service Name" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
-                        </div>                        
+                                placeholder="Enter Service Name" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required />
+                            {errors.name && <p className="text-red-500">{errors.name}</p>}
+                        </div>
                         <div className=' mt-3'>
                             <h3 className='font-medium mb-1'>Description</h3>
                             <textarea id="Updateemail" name="Updateemail" value={description} onChange={(e) => setDescription(e.target.value)}
-                            placeholder="Enter description" className="min-h-20 max-h-20 emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-1/2" required></textarea>
+                                placeholder="Enter description" className="min-h-20 max-h-20 emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-1/2" required></textarea>
+                            {errors.description && <p className="text-red-500">{errors.description}</p>}
                         </div>
                         <div className='w-2/5 mt-1'>
                             <h3 className='font-medium mb-1'>Gitlab url</h3>
                             <input type="text" id="Updateemail" name="Updateemail" value={gitlabUrl} onChange={(e) => setGitlabUrl(e.target.value)}
-                            placeholder="Enter link" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
+                                placeholder="Enter link" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required />
+                            {errors.gitlabUrl && <p className="text-red-500">{errors.gitlabUrl}</p>}
                         </div>
                         <div className='mt-3 flex justify-center w-1/2'>
                             <div className='w-4/5'>
                                 <h3 className='font-medium mb-1'>Pre-Prod URL</h3>
                                 <input type="text" id="Updateemail" name="Updateemail" value={preprodUrl} onChange={(e) => setPreprodUrl(e.target.value)}
-                                placeholder="Enter link" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
+                                    placeholder="Enter link" className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required />
+                                {errors.preprodUrl && <p className="text-red-500">{errors.preprodUrl}</p>}
                             </div>
                             <div className='w-1/5'>
                                 <p className='mb-2 ml-1 font-medium'>
                                     Status
                                 </p>
                                 <label className='autoSaverSwitch relative inline-flex cursor-pointer select-none items-center'>
-                                    <input type='checkbox' name='autoSaver' className='sr-only' checked={isChecked} onChange={handleCheckboxChange} />
-                                    <span className={`slider mr-3 flex h-[26px] w-[50px] items-center rounded-full p-1 duration-200 ${ isChecked ? 'bg-slate/900' : 'bg-[#CCCCCE]' }`}>
+                                    <input type='checkbox' name='autoSaver' className='sr-only' checked={isChecked} onChange={handleCheckboxPreProd} />
+                                    <span className={`slider mr-3 flex h-[26px] w-[50px] items-center rounded-full p-1 duration-200 ${isChecked ? 'bg-slate/900' : 'bg-[#CCCCCE]'}`}>
                                         <span
-                                            className={`dot h-[18px] w-[18px] rounded-full bg-white duration-200 ${
-                                            isChecked ? 'translate-x-6' : ''
-                                            }`}>
+                                            className={`dot h-[18px] w-[18px] rounded-full bg-white duration-200 ${isChecked ? 'translate-x-6' : ''}`}>
                                         </span>
                                     </span>
                                     <span className='label flex items-center text-sm font-medium text-black'>
@@ -169,19 +182,18 @@ const Page = () => {
                             <div className='w-4/5'>
                                 <h3 className='font-medium mb-1'>Production URL</h3>
                                 <input type="text" id="Updateemail" name="Updateemail" placeholder="Enter link" value={prodUrl} onChange={(e) => setProdUrl(e.target.value)}
-                                className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required/>
+                                    className="emailcustom placeholder:opacity-50 py-3 px-4 rounded-md border-2 border-solid border-neutral-300 focus:outline-none w-4/5" required />
+                                {errors.prodUrl && <p className="text-red-500">{errors.prodUrl}</p>}
                             </div>
                             <div className='w-1/5'>
                                 <p className='mb-2 ml-1 font-medium'>
                                     Status
                                 </p>
                                 <label className='autoSaverSwitch relative inline-flex cursor-pointer select-none items-center'>
-                                    <input type='checkbox' name='autoSaver' className='sr-only' checked={isChecked2} onChange={handleCheckboxChange2} />
-                                    <span className={`slider mr-3 flex h-[26px] w-[50px] items-center rounded-full p-1 duration-200 ${ isChecked2 ? 'bg-slate/900' : 'bg-[#CCCCCE]' }`}>
+                                    <input type='checkbox' name='autoSaver' className='sr-only' checked={isChecked2} onChange={handleCheckboxProd} />
+                                    <span className={`slider mr-3 flex h-[26px] w-[50px] items-center rounded-full p-1 duration-200 ${isChecked2 ? 'bg-slate/900' : 'bg-[#CCCCCE]'}`}>
                                         <span
-                                            className={`dot h-[18px] w-[18px] rounded-full bg-white duration-200 ${
-                                            isChecked2 ? 'translate-x-6' : ''
-                                            }`}>
+                                            className={`dot h-[18px] w-[18px] rounded-full bg-white duration-200 ${isChecked2 ? 'translate-x-6' : ''}`}>
                                         </span>
                                     </span>
                                     <span className='label flex items-center text-sm font-medium text-black'>
@@ -195,14 +207,14 @@ const Page = () => {
                         </div>
                         <div className='mt-8 flex justify-between pr-16'>
                             <p className='active:scale-95 min-w-16 form-flex justify-center items-center py-3 px-4 gap-2 cursor-pointer rounded-md shadow-sm text-red-600 bg-white border border-red-600 w-20 mt-3 mb-1 ml-3 font-semibold text-center' onClick={() => router.back()}>Cancel</p>
-                            <input type="submit" value="Save" className="active:scale-95 min-w-16 form-flex justify-center items-center border py-3 px-4 gap-2 cursor-pointer rounded-md shadow-sm text-white bg-red-600 w-20 mt-3 mb-1 ml-3 font-semibold" 
-                                onClick={createSubmit}
+                            <input type="submit" value="Save" className="active:scale-95 min-w-16 form-flex justify-center items-center border py-3 px-4 gap-2 cursor-pointer rounded-md shadow-sm text-white bg-red-600 w-20 mt-3 mb-1 ml-3 font-semibold"
                             />
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-    )
+    );
+        
 }
 export default Page;

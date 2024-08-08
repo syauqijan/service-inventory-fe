bd7f6f35d8789ffdf87ce7be82ca090fc2aae26b
syauqijan <13521014@std.stei.itb.ac.id>
2024-08-08 13:02:02 +0700

    feat: handle protected page

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import {jwtDecode} from 'jwt-decode';
+import axios from 'axios';
 
 interface JwtPayload {
     email: string;
@@ -9,7 +10,7 @@ interface JwtPayload {
     userId: number;
 }
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
     const token = req.cookies.get("authToken");
 
     if (token) {
@@ -20,10 +21,18 @@ export function middleware(req: NextRequest) {
                 return NextResponse.redirect(new URL("/login", req.url));
             }
 
+
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_API_ENDPOINT_USERS}/${decoded.userId}`);
+            const userRole = response.data;
+
+            if (req.nextUrl.pathname === '/dashboard/user-management' && userRole.roleId !== 1) {
+                return NextResponse.redirect(new URL("/dashboard", req.url));
+            }
+
             return NextResponse.next();
             
         } catch (error) {
-            console.error("Token decode error:", error);
+            console.error("Token decode or role check error:", error);
             return NextResponse.redirect(new URL("/login", req.url));
         }
     } else {
@@ -32,5 +41,8 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: "/((?!api|_next/static|_next/image|favicon.ico|login).*)",
+    matcher: [
+        '/dashboard',
+        '/dashboard/user-management'
+    ],
 };
